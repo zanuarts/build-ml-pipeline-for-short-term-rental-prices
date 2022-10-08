c4682d9cb9d28afbc2bca20ac3f7072b2521de31
Zanuar <zanuar.er@gmail.com>
2022-10-08 20:29:44 +0700

    add test model component

--- main.py
+++ main.py
@@ -129,8 +129,14 @@ def go(config: DictConfig):
             ##################
             # Implement here #
             ##################
-
-            pass
+            _ = mlflow.run(
+                f"{config['main']['components_repository']}/test_regression_model",
+                "main",
+                parameters={
+                    "mlflow_model": "random_forest_export:prod",
+                    "test_dataset": "test_data.csv:latest"
+                },
+            )
 
 
 if __name__ == "__main__":
