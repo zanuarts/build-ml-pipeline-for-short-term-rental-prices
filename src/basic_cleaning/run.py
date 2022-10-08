44e22f03dd651233d0ad65d7599aadf6b5e00194
Zanuar <zanuar.er@gmail.com>
2022-10-08 20:58:08 +0700

    fix test proper boundaries

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -40,6 +40,9 @@ def go(args):
     df['last_review'] = pd.to_datetime(df['last_review'])
 
     logger.info("Save result")
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+
     filename = "clean_sample.csv"
     df.to_csv(filename, index=False)
 
