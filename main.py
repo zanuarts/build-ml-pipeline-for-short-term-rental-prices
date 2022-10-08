59c2e33a2776a93a5a2c9251cb90e02dd61b381a
Zanuar <zanuar.er@gmail.com>
2022-10-08 20:26:01 +0700

    add train component

--- main.py
+++ main.py
@@ -109,8 +109,20 @@ def go(config: DictConfig):
             ##################
             # Implement here #
             ##################
+            _ = mlflow.run(
+                os.path.join(hydra.utils.get_original_cwd(), "src", "train_random_forest"),
+                "main",
+                parameters={
+                    "trainval_artifact": "trainval_data.csv:latest",
+                    "val_size": config["modeling"]["test_size"],
+                    "random_seed": config["modeling"]["random_seed"],
+                    "stratify_by": config["modeling"]["stratify_by"],
+                    "rf_config": rf_config,
+                    "max_tfidf_features": config["modeling"]["max_tfidf_features"],
+                    "output_artifact": "random_forest_export"
+                },
+            )
 
-            pass
 
         if "test_regression_model" in active_steps:
 
