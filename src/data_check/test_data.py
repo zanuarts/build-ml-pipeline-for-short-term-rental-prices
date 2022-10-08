ad4bd8a8cac63406c7881f255963c47bf561aa96
Zanuar <zanuar.er@gmail.com>
2022-10-08 14:20:05 +0700

    add test to data check pipeline

--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -63,3 +63,14 @@ def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_th
 ########################################################
 # Implement here test_row_count and test_price_range   #
 ########################################################
+def test_row_count(data):
+    """
+    Test to checks that the size of the dataset is reasonable (not too small, not too large).
+    """
+    assert 15000 < data.shape[0] < 1000000
+
+def test_price_range(data, min_price, max_price):
+    """
+    Test to checks that the price range is between min_price and max_price
+    """
+    assert data['price'].between(min_price, max_price)
